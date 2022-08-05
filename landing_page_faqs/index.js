015b41a7d641a83135b67a0c3c77cfe1e4ee2e2d
Osilaja78 <osilajaabdulhameed@gmail.com>
2022-08-05 23:02:32 +0100

    nothing much

--- landing_page_faqs/index.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-}
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
