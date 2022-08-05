4288849689984a6b4649b3a06a132395472acf70
Osilaja78 <osilajaabdulhameed@gmail.com>
2022-08-05 23:00:41 +0100

    linked landing page and faqs page

--- apps/common/urls.py
+++ apps/common/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView, name='homepage'),
     path('about/', views.AboutView, name='about_us'),
+    path('FAQs/', views.FAQsView, name='FAQs'),
 ]
--- apps/common/views.py
+++ apps/common/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def HomeView(request):
+    return render(request, "common/index.html")
+
 def AboutView(request):
     return render(request, "common/about.html")
 
+def FAQsView(request):
+    return render(request, "common/FAQs.html")
