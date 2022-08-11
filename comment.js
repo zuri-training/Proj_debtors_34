4a89e93af6d1efc9d4f7c0287aaeddb50239b4c1
shaksworld <shakaaliakwe>
2022-08-11 17:48:27 +0100

    comment page

---
+++ comment.js
@@ -0,0 +1,8 @@
+// var profile =  document.querySelectorAll('#profile');
+// var main = document.querySelectorAll('#add-content');
+
+// for(i =0; i < profile.length; i++){
+//     profile[i].style.width = '40px';
+// }
+// console.log(profile);
+// console.log(main)
